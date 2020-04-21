566a6dbea17b45a2cfe24403f607fd67c132bd1e
unknown <evan.mead@yahoo.com>
2020-04-21 10:51:00 -0500

    Views-Index/Create created

--- Superheroes/Controllers/SuperheroController.cs
+++ Superheroes/Controllers/SuperheroController.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Superheroes.Data;
+using Superheroes.Models;
 
 namespace Superheroes.Controllers
 {
     public class SuperheroController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: Superhero
         public ActionResult Index()
         {
-            return View();
+            var superheros = _context.Superheroes.ToList();
+            return View(superheros);
         }
 
         // GET: Superhero/Details/5
@@ -30,17 +40,13 @@ namespace Superheroes.Controllers
         // POST: Superhero/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Superhero superhero)
         {
-            try
+            // TODO: Add insert logic here
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
+                _context.Superheroes.Add(superhero);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
             }
         }
 
@@ -53,15 +59,9 @@ namespace Superheroes.Controllers
         // POST: Superhero/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Superhero superhero)
         {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+            // TODO: Add update logic here
             {
                 return View();
             }
@@ -76,15 +76,9 @@ namespace Superheroes.Controllers
         // POST: Superhero/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Superhero superhero)
         {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+            // TODO: Add delete logic here
             {
                 return View();
             }
